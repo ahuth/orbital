c3de94355ce92a3217a77cbe4eb47cf22e7bdc25
Andrew Huth <andrew@huth.me>
2017-05-06 22:13:47 -0700

    Extract <Space> component

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,16 @@
 import React from "react"
-import {Circle, Layer, Rect, Stage} from "react-konva"
+import Space from "./space"
+import {Circle} from "react-konva"
 
 export default function App() {
   return (
-    <Stage height={700} width={700}>
-      <Layer>
-        <Rect
-          x={0}
-          y={0}
-          width={700}
-          height={700}
-          fill="#191919"
-        />
-        <Circle
-          x={350}
-          y={350}
-          radius={20}
-          fill="red"
-        />
-      </Layer>
-    </Stage>
+    <Space>
+      <Circle
+        x={350}
+        y={350}
+        radius={20}
+        fill="red"
+      />
+    </Space>
   )
 }
