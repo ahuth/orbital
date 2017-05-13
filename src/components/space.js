6a885e34f499d8909cb96fd76fe3cbaf807f16e2
Andrew Huth <andrew@huth.me>
2017-05-13 10:47:02 -0700

    Render based on processed children instead of a component's direct children
    This sets the stage for us to do physics calculations and update the
    positions of the children on the canvas.

--- src/components/space.js
+++ src/components/space.js
@@ -3,12 +3,33 @@ import PropTypes from "prop-types"
 import {Layer, Rect, Stage} from "react-konva"
 
 export default class Space extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bodies: React.Children.map(props.children, this.processChild)
+    }
+  }
+
   componentWillMount() {
     this.context.loop.subscribe(this.update)
   }
 
   componentWillUnmount() {
     this.context.loop.unsubscribe(this.update)
+  }
+
+  processChild(child) {
+    return {
+      component: child,
+      x: child.props.x,
+      y: child.props.y
+    }
+  }
+
+  processedChildren() {
+    return this.state.bodies.map((child, index) => {
+      return React.cloneElement(child.component, {x: child.x, y: child.y, key: index})
+    })
   }
 
   update(time) {
@@ -28,7 +49,7 @@ export default class Space extends React.Component {
           />
         </Layer>
         <Layer>
-          {this.props.children}
+          {this.processedChildren()}
         </Layer>
       </Stage>
     )
