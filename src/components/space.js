a2cdba484c3bd4c764b1d1b8a488b0dc7f3b3eec
Andrew Huth <andrew@huth.me>
2017-05-12 22:04:02 -0700

    Use a class component for the <Space>

--- src/components/space.js
+++ src/components/space.js
@@ -1,21 +1,23 @@
 import React from "react"
 import {Layer, Rect, Stage} from "react-konva"
 
-export default function Space({children}) {
-  return (
-    <Stage height={700} width={700}>
-      <Layer>
-        <Rect
-          x={0}
-          y={0}
-          width={700}
-          height={700}
-          fill="#191919"
-        />
-      </Layer>
-      <Layer>
-        {children}
-      </Layer>
-    </Stage>
-  )
+export default class Space extends React.Component {
+  render() {
+    return (
+      <Stage height={700} width={700}>
+        <Layer>
+          <Rect
+            x={0}
+            y={0}
+            width={700}
+            height={700}
+            fill="#191919"
+          />
+        </Layer>
+        <Layer>
+          {this.props.children}
+        </Layer>
+      </Stage>
+    )
+  }
 }
